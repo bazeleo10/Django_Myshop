6163be87105f84bdd3657338c12a722024413e40
bazeleo10 <70655706+bazeleo10@users.noreply.github.com>
2022-09-01 11:11:08 +0530

    Add files via upload

---
+++ pythonFinal/products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product
+
+class productAdmin(admin.ModelAdmin):
+    list_display =('name','price','stock')
+
+admin.site.register(Product,productAdmin)
+
